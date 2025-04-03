1045789f28b2428e316fb68881c4bb6a8d8a5178
Lechros <0onionion0@gmail.com>
2025-04-03 23:17:13 +0900

    chore: remove Spring Data Redis and add Redisson dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.redisson:redisson-spring-boot-starter:3.45.1")
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-mysql")
     implementation("com.linecorp.kotlin-jdsl:jpql-dsl:3.5.5")
